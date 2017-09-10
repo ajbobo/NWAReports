ae2881f373b60b916d525be28a167ae310328e4f
ajbobo <ajbobo@users.noreply.github.com>
2017-09-10 13:43:56 -0600

    Improved formatting of Util.printTable()

--- src/test/java/org/noahwebster/nwareports/Util.java
+++ src/test/java/org/noahwebster/nwareports/Util.java
@@ -2,10 +2,7 @@ package org.noahwebster.nwareports;
 
 import org.noahwebster.nwareports.types.StringRow;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Util {
 	public static void printTable(DataTable table) {
@@ -14,16 +11,19 @@ public class Util {
 
 	public static void printTable(DataTable table, int limit) {
 		if (table.getData() != null) {
+			Map<String, String> formats = getColumnFormats(table.getData());
 			List<StringRow> theTable = table.getData();
 			Set<String> columns = theTable.get(0).columnNames();
+			System.out.print("|");
 			for (String colName : columns)
-				System.out.print(colName + "\t");
+				System.out.print(String.format(formats.get(colName), colName));
 			System.out.println();
 			Iterator<StringRow> iterator = theTable.iterator();
 			for (int x = 0; (limit == 0 || x < limit) && iterator.hasNext(); x++) {
 				Map<String, String> row = iterator.next();
+				System.out.print("|");
 				for (String colName : columns)
-					System.out.print(row.get(colName) + "\t");
+					System.out.print(String.format(formats.get(colName), row.get(colName)));
 				System.out.println();
 			}
 			System.out.println("Total Rows: " + table.getData().size());
@@ -33,5 +33,27 @@ public class Util {
 			for (String msg : table.getErrors())
 				System.out.println(msg);
 		}
+	}
+
+	private static Map<String, String> getColumnFormats(List<StringRow> data) {
+		LinkedHashMap<String, Integer> widths = new LinkedHashMap<>();
+		for (String colName : data.get(0).columnNames()) {
+			widths.put(colName, colName.length());
+		}
+
+		for (StringRow row : data) {
+			for (String col : row.columnNames()) {
+				String val = row.get(col);
+				if (val.length() > widths.get(col))
+					widths.put(col, val.length());
+			}
+		}
+
+		LinkedHashMap<String, String> formats = new LinkedHashMap<>();
+		for (String col : widths.keySet()) {
+			formats.put(col, String.format(" %%-%ds |", widths.get(col)));
+		}
+
+		return formats;
 	}
 }
